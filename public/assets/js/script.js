a43965201ef2993072d2074930d08adaa14bf7ab
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-10-17 06:18:32 +0700

    Update Login Page

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,39 +4,36 @@ const nextBtn = document.querySelector('.next');
 let currentSlide = 0;
 let slideInterval;
 
-// Hiển thị slide theo chỉ số
+
 function showSlide(index) {
   slides.forEach(slide => slide.classList.remove('active'));
   slides[index].classList.add('active');
 }
 
-// Chuyển về slide trước đó
 prevBtn.addEventListener('click', () => {
   currentSlide = (currentSlide === 0) ? slides.length - 1 : currentSlide - 1;
   showSlide(currentSlide);
   resetAutoSlide();
 });
 
-// Chuyển sang slide kế tiếp
+
 nextBtn.addEventListener('click', () => {
   currentSlide = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
   showSlide(currentSlide);
   resetAutoSlide();
 });
 
-// Tự động chuyển slide
 function autoSlide() {
   slideInterval = setInterval(() => {
     currentSlide = (currentSlide === slides.length - 1) ? 0 : currentSlide + 1;
     showSlide(currentSlide);
-  }, 3000); // Chuyển sau mỗi 3 giây
+  }, 3000); 
 }
 
-// Reset lại thời gian tự động khi người dùng thao tác với nút điều hướng
+
 function resetAutoSlide() {
   clearInterval(slideInterval);
   autoSlide();
 }
 
-// Bắt đầu tự động chạy slide khi tải trang
 autoSlide();
